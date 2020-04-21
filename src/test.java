0304b59c3e9aafed8acb9f74b89642c0febd0519
fengyan <506845408@qq.com>
2020-04-21 22:39:23 +0800

    ceshi

---
+++ src/test.java
@@ -0,0 +1,14 @@
+import sun.awt.geom.AreaOp;
+import java.awt.*;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println();
+        Frame f = new Frame();
+        f.setSize(800,600);
+        f.setLocation(20,10);
+        f.setVisible(true);
+
+    }
+}
